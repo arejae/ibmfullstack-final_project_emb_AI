2423c16e1c2fbe229858908f09928a9520f8d06f
Azam Abdullah <arejae@gmail.com>
2024-10-20 14:32:14 -0400

    final update

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,10 +1,22 @@
 import requests
 import json
+
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
+    if response.status_code == 400:
+        output_res = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        return output_res
+
     response_in_json = json.loads(response.text)
     emotions = response_in_json['emotionPredictions'][0]['emotion']
     dominant_emotion = max(emotions, key=emotions.get)
--- server.py
+++ server.py
@@ -1,19 +1,36 @@
+"""
+This module provides a Flask-based web application for emotion detection.
+It includes endpoints for analyzing emotions in a given text and rendering an index page.
+"""
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
-app = Flask('Emotion Detector')
+
+app = Flask(__name__)
+
 @app.route('/emotionDetector')
 def emotion_analyser():
+    """Analyzes the emotion in the provided text."""
     text_to_analyse = request.args.get('textToAnalyze')
     results_of_analysis = emotion_detector(text_to_analyse)
-    if results_of_analysis is None:
+
+    if not results_of_analysis or not results_of_analysis.get('dominant_emotion'):
         return "Invalid input! Try again."
-    return f"For the given statement, the system response is\
-     'anger': {results_of_analysis['anger']}, 'disgust': {results_of_analysis['disgust']},\
-      'fear': {results_of_analysis['fear']}, 'joy': {results_of_analysis['joy']}\
-       and 'sadness': {results_of_analysis['sadness']}.\
-        The dominant emotion is {results_of_analysis['dominant_emotion']}."
+
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {results_of_analysis['anger']}, "
+        f"'disgust': {results_of_analysis['disgust']}, "
+        f"'fear': {results_of_analysis['fear']}, "
+        f"'joy': {results_of_analysis['joy']}, "
+        f"and 'sadness': {results_of_analysis['sadness']}. "
+        f"The dominant emotion is {results_of_analysis['dominant_emotion']}."
+    )
+
 @app.route("/")
 def render_index_page():
+    """Renders the index page."""
     return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(port=5000)
